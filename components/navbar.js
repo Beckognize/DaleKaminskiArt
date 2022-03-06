f97ce366eb5724268af1823d164b2113b5389819
Rebecca Lessem <beckognize@gmail.com>
2022-03-06 16:46:08 +0000

    Added images and adjusted navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,26 +3,55 @@ import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
 export default () => {
     
     return (
-        
-<Navbar bg="light" expand="lg">
-    <div className= 'container-fluid'>
-        <Navbar.Brand href="#home">Dale Kaminski</Navbar.Brand>
-    </div>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <NavDropdown title="Work" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Landscapes</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Twisted Figures</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Interactive Gallery</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-</Navbar>
-     
-     );
-    }
+  <Navbar bg="light" expand="lg">
+    <Container fluid>
+      <Navbar.Brand href="#home">Dale Kaminski</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="justify-content-end">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <NavDropdown title="Work" id="basic-nav-dropdown" align="end">
+            <NavDropdown.Item href="#action/3.1">Landscapes</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Twisted Figures</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Interactive Gallery</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+     </Container>
+   </Navbar>
+  );
+}
+ 
+/*<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Dale Kaminski</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav> */ 
+    
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,34 @@ export default () => {
 
 <Header/>
 <Navbar/>
-
+<body>
+    <div className = 'container-fluid'>
+    <div className ='photobox'>
+        {/*
+        <img className= 'jump-image'style={{ top: "10%", right: "20%",}} src='images/DawnV_1.jpg' alt='woman jumping'></img>
+        <img className= 'small-image'style={{ top: "36%", right: "37%" }} src='images/Bee_polen_flower.jpg' alt='flower and bee'></img>
+        <img className= 'small-image'style={{ top: "36%", left: "50%" }} src='images/lotus.jpg' alt='woman jumping'></img>
+        <img className= 'med-image'style={{ top: "90%", left: "20%" }} src='images/blackBird_contrail.jpg' alt='airplane'></img>
+        <img className= 'small-image'style={{ top: "15%", left: "25%" }} src='images/Hail_Badger.jpg' alt='woman jumping'></img>
+        <img className= 'flag-image'style={{ top: "10%", left: "15%" }} src='images/Mainstreet_flags_america.jpg' alt='woman jumping'></img>
+        <img className= 'med-image'style={{ top: "65%", right: "33%" }} src='images/reeds_and_Pond.jpg' alt='woman jumping'></img>
+        <img className= 'med-image'style={{ top: "65%", right: "30%" }} src='images/windmills_sky_atmosphere.jpg' alt='woman jumping'></img>
+        <img className= 'medsmall-image'style={{ top: "65%", right: "50%" }} src='images/three ladies_on bench.jpg' alt='woman jumping'></img>
+        <img className= 'med-image'style={{ top: "65%", right: "6%" }} src='images/Wuhan_rags and riches.jpg' alt='woman jumping'></img>
+        */}
+        <img style={{ top: "10%", right: "18%", width: '15%' }} src='images/Jumpthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "30%", right: "35%", width: '7%' }} src='images/bflowerthumb.jpg' alt='flower and bee'></img>
+        <img style={{ top: "30%", left: "50%", width: '7%' }} src='images/lotusthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "15%", left: "25%", width: '7%' }} src='images/Hailbadgerthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "10%", left: "17%", width: '7%' }} src='images/flagsthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "30%", left: "4%", width: '10%' }} src='images/ladiesthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "50%", left: "16%", width: '13%' }} src='images/blackBirdthumb.jpg' alt='airplane'></img>
+        <img style={{ top: "38%", left: "30%", width: '13%' }} src='images/windmillsthumb.jpg' alt='woman jumping'></img> 
+        <img style={{ top: "55%", right: "28%", width: '13%' }} src='images/wuhanthumb.jpg' alt='woman jumping'></img>
+        <img style={{ top: "40%", right: "3%", width: '11%' }} src='images/reedsthumb.jpg' alt='woman jumping'></img>   
+    </div>
+    </div>
+</body>
 </>
 );
 }
