82592036f0c74636c58913cae1b51bd80d150edb
Rebecca Lessem <beckognize@gmail.com>
2022-04-01 13:55:58 +0100

    Added images to america page

--- pages/america.js
+++ pages/america.js
@@ -3,7 +3,94 @@ import Navbar from '../components/navbar'
 import Header from '../components/header'
 import Slider from '../components/slider'
 
-  const myImages = []
+const myImages = [
+  {
+    src:'images/america/cloudflag.jpg',
+    alt:'american flag in stormy weather',
+    style:{ top: "0%", left: "0%", width: '28%' }
+  },
+  {
+    src:'images/america/flagbar1.jpg',
+    alt:'American decorations on bar in resturant',
+    style:{ top: "0%", left: "29%", width: '25%' }
+  },
+  {
+    src: 'images/america/flagbar2.jpg',
+    alt: 'American decorations on wall in bar',
+    style: { top: "0%", right: "30%", width: '15%' }
+  },
+  {
+    src:'images/america/bonfire.jpg' ,
+    alt:'A bonfire on the beach with a band',
+    style: { top: "0%", right: "15%", width: '15%' }
+  },
+  {
+    src:'images/america/flagbar3.jpg' ,
+    alt:'American flag on bar wall with writing on it',
+    style: { top: "0%", right: "0%", width: '15%' }
+  },
+  {
+    src:'images/america/caution.jpg',
+    alt:'caution tape on baby swings',
+    style:{ top: "26%", left: "0%", width: '13%' }
+  },
+  {
+    src:'images/america/caution2.jpg',
+    alt:'caution tape on baby swings',
+    style:{ top: "26%", left: "15%", width: '15%' }
+  },
+  {
+    src:'images/america/fall.jpg',
+    alt:'man on ground woman on phone',
+    style:{ top: "26%", left: "33%", width: '16%' }
+  },
+  {
+    src:'images/america/jesuscar.jpg',
+    alt:'car with religious bumper stickers',
+    style:{ top: "27%", right: "36%", width: '14%' }
+  },
+  {
+    src:'images/america/bowpeeps.jpg',
+    alt:'people on the bow of a boat',
+    style:{top: "30%", right: "23%", width: '11%' }
+  },
+  {
+    src:'images/america/truck.jpg',
+    alt:'semi truck on overpass',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+   {
+    src:'images/america/flags.jpg',
+    alt:'pickup truck with many american flags',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/america/caution3.jpg',
+    alt:'caution tape on baby swing',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/america/animals.jpg',
+    alt:'Stuffed animals on rocks',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/america/tractor.jpg',
+    alt:'tractor on road',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/america/powerstation.jpg',
+    alt:'people swimming on beach next to power station',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/america/diner.jpg',
+    alt:'wide shot of inside of american diner',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+]
+
         
             
             
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default () => {
         <Link href='/america'><a><img style={{ top: "8%", left: "10%", width: '18%' }} src='images/MainStreet_flags_america.jpg' alt='pickup truck with many american flags'></img></a></Link>
         <Link href='/selfportrait'><a><img style={{ top: "45%", left: "2%", width: '18%' }} src='images/shiny_bug_selfPortrait-6.jpg' alt='reflection in shiny beatle shell'></img></a></Link>
         <Link href='/graphic'><a><img style={{ top: "70%", left: "16%", width: '15%' }} src='images/blackBird_contrail.jpg' alt='black bird sky and airplane contrail'></img></a></Link>
-        <Link href='/topographic'><a><img style={{ top: "53%", left: "40%", width: '25%' }} src='images/windmills_sky_atmosphere.jpg' alt='windmills and clouds'></img></a></Link>
+        <Link href='/topographics'><a><img style={{ top: "53%", left: "40%", width: '25%' }} src='images/windmills_sky_atmosphere.jpg' alt='windmills and clouds'></img></a></Link>
         <Link href='/travels'><a><img style={{ top: "73%", right: "28%", width: '15%' }} src='images/Wuhan_rags and riches.jpg' alt='alley in Wuhan'></img></a></Link>
         <Link href='/art'><a><img style={{ top: "50%", right: "3%", width: '13%' }} src='images/reeds_and_Pond.jpg' alt='reeds in a pond'></img></a></Link>   
     </div>
