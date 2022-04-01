0784655f506bc04827e733141075b3e2dacceeb9
Rebecca Lessem <beckognize@gmail.com>
2022-04-01 13:28:00 +0100

    Added images to new pages

--- pages/flower.js
+++ pages/flower.js
@@ -3,7 +3,104 @@ import Navbar from '../components/navbar'
 import Header from '../components/header'
 import Slider from '../components/slider'
 
-  const myImages = []
+const myImages = [
+  {
+    src:'images/flower/Alaskaflower.jpg',
+    alt:'yellow flower',
+    style:{ top: "0%", left: "0%", width: '28%' }
+  },
+  {
+    src:'images/flower/beeflower.jpg',
+    alt:'bee with pollen on flower',
+    style:{ top: "0%", left: "29%", width: '25%' }
+  },
+  {
+    src: 'images/flower/flowerbridge.jpg',
+    alt: 'bridge with flowers',
+    style: { top: "0%", right: "30%", width: '15%' }
+  },
+  {
+    src:'images/flower/snowtree.jpg' ,
+    alt:'tree with snow by lake',
+    style: { top: "0%", right: "15%", width: '15%' }
+  },
+  {
+    src:'images/flower/waterdrops.jpg' ,
+    alt:'flower with water droplets',
+    style: { top: "0%", right: "0%", width: '15%' }
+  },
+  {
+    src:'images/flower/hangpod.jpg',
+    alt:'seed pod in sky',
+    style:{ top: "26%", left: "0%", width: '13%' }
+  },
+  {
+    src:'images/flower/jewel.jpg',
+    alt:'red small flowers',
+    style:{ top: "26%", left: "15%", width: '15%' }
+  },
+  {
+    src:'images/flower/lotus.jpg',
+    alt:'lotus blossom',
+    style:{ top: "26%", left: "33%", width: '16%' }
+  },
+  {
+    src:'images/flower/dragonfly.jpg',
+    alt:'dragonfly on leaf',
+    style:{ top: "27%", right: "36%", width: '14%' }
+  },
+  {
+    src:'images/flower/closeup.jpg',
+    alt:'closeup of flower',
+    style:{top: "30%", right: "23%", width: '11%' }
+  },
+  {
+    src:'images/flower/pinecone.jpg',
+    alt:'closeup pine cone flower',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+   {
+    src:'images/flower/tallbush.jpg',
+    alt:'tall bush with trees',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/flower/twist.jpg',
+    alt:'twisted wood in desert',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/flower/twist2.jpg',
+    alt:'twisted wood in desert',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+ {
+    src:'images/flower/monarch.jpg',
+    alt:'monarch butterfly on green flower',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/flower/peony-22.jpg',
+    alt:'peony flower',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/flower/resin.jpg',
+    alt:'drop of resin on leaf',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/flower/thistle-1.jpg',
+    alt:'thistle flower',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+{
+    src:'images/flower/wisteria-14.jpg',
+    alt:'wisteria plant',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+]
+
         
             
             
--- pages/topographics.js
+++ pages/topographics.js
@@ -3,7 +3,65 @@ import Navbar from '../components/navbar'
 import Header from '../components/header'
 import Slider from '../components/slider'
 
-  const myImages = []
+const myImages = [
+  {
+    src:'images/topographics/coopsilo.jpg',
+    alt:'grain silo with coop on it',
+    style:{ top: "0%", left: "0%", width: '28%' }
+  },
+  {
+    src:'images/topographics/grainsilos1.jpg',
+    alt:'grain silo',
+    style:{ top: "0%", left: "29%", width: '25%' }
+  },
+  {
+    src: 'images/topographics/small_lighthouse.jpg',
+    alt: 'small lighthouse in distance',
+    style: { top: "0%", right: "30%", width: '15%' }
+  },
+  {
+    src:'images/topographics/3electrictower.jpg' ,
+    alt:'three electric towers',
+    style: { top: "0%", right: "15%", width: '15%' }
+  },
+  {
+    src:'images/topographics/silosquare.jpg' ,
+    alt:'grain silo',
+    style: { top: "0%", right: "0%", width: '15%' }
+  },
+  {
+    src:'images/topographics/silosugar.jpg',
+    alt:'sugar silo',
+    style:{ top: "26%", left: "0%", width: '13%' }
+  },
+  {
+    src:'images/topographics/odetosilo.jpg',
+    alt:'grain silo',
+    style:{ top: "26%", left: "15%", width: '15%' }
+  },
+  {
+    src:'images/topographics/odetosilo9.jpg',
+    alt:'grain silo',
+    style:{ top: "26%", left: "33%", width: '16%' }
+  },
+  {
+    src:'images/topographics/topocortez.jpg',
+    alt:'landscape',
+    style:{ top: "27%", right: "36%", width: '14%' }
+  },
+  {
+    src:'images/topographics/stopsigns.jpg',
+    alt:'group of stop signs',
+    style:{top: "30%", right: "23%", width: '11%' }
+  },
+  {
+    src:'images/topographics/windmills.jpg',
+    alt:'windmills across sky',
+    style:{top: "27%", right: "0%", width: '20%' }
+  },
+  
+]
+
         
             
             
