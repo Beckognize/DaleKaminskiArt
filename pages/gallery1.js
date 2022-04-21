6e7303df5bd33f8768063a40cc4a77d05731beae
Rebecca Lessem <beckognize@gmail.com>
2022-04-21 15:46:30 +0100

    Recongfigured images on pages

--- pages/gallery1.js
+++ pages/gallery1.js
@@ -11,95 +11,66 @@ const myImages = [
     style:{top: "0%", left: "0%", width: '27%' }
   },
   {
-    src:'images/america/truck.jpg',
-    alt:'semi truck on overpass',
-    caption:'Black Mystery Truck',
-    style:{top: "3%", right: "40%", width: '32%' }
-  },
-  {
-    src:'images/america/caution3.jpg',
-    alt:'caution tape on baby swing',
-    caption:'Kiddie Swing Police Line',
-    style:{top: "0%", right: "25%", width: '15%' }
-  },
-  {
     src:'images/america/flatroad.jpg',
     alt:'flat road panorama',
     caption:'"Road", Rural Kansas.',
-    style:{ top: "20%", right: "22%", width: '52%' }
+    style:{ top: "2%", left: "30%", width: '60%' }
   },
-  {
-    src:'images/america/jesuscar.jpg',
-    alt:'car with religious bumper stickers',
-    caption:'Riding with Jesus',
-    style:{ top: "16%", left: "0%", width: '25%' }
-  },
-  {
-    src:'images/america/tractor.jpg',
-    alt:'tractor on road',
-    caption:'Rake',
-    style:{top: "0%", right: "0%", width: '20%' }
-  },
-  {
-    src:'images/america/fall.jpg',
-    alt:'man on ground woman on phone',
-    caption:'Calling it in',
-    style:{ top: "46%", right: "0%", width: '20%' }
-  },
-  {
-    src:'images/america/powerstation.jpg',
-    alt:'people swimming on beach next to power station',
-    caption:'"Industrial Swimming," Dunkirk, NY.',
-    style:{top: "60%", left: "25%", width: '25%' }
-  },
-  {
-    src:'images/america/cloudflag.jpg',
-    alt:'american flag in stormy weather',
-    caption:'Camping World Giant Flag',
-    style:{ top: "60%", left: "50%", width: '30%' }
-  },
-  {
-    src:'images/america/animals.jpg',
-    alt:'Stuffed animals on rocks',
-    caption:'Hyper-reality',
-    style:{top: "46%", left: "0%", width: '25%' }
-  },
-  {
-    src:'images/america/bonfire.jpg' ,
-    alt:'A bonfire on the beach with a band',
-    caption:'"BonFire Beach," Luddington, Michigan.',
-    style: { top: "85%", left: "25%", width: '26%' }
-  },
-  {
-    src:'images/america/bowpeeps.jpg',
-    alt:'people on the bow of a boat',
-    caption:'Deck of the Badger Crossing Lake Michigan',
-    style:{top: "78%", right: "21%", width: '27%' }
-  },
-  {
+   {
     src:'images/america/flagbar1.jpg',
     alt:'American decorations on bar in resturant',
     caption:'"Right Wing Dinner," Ohio',
-    style:{ top: "30%", left: "11%", width: '25%' }
+    style:{ top: "25%", left: "11%", width: '25%' }
   },
   {
     src: 'images/america/flagbar2.jpg',
     alt: 'American decorations on wall in bar',
     caption:'"Right Wing Dinner 2," Ohio',
-    style: { top: "30%", right: "37%", width: '25%' }
+    style: { top: "25%", right: "37%", width: '25%' }
   },
   {
     src:'images/america/flagbar3.jpg' ,
     alt:'American flag on bar wall with writing on it',
     caption:'"Right Wing Dinner 3," Ohio',
-    style: { top: "30%", right: "10%", width: '25%' }
-  },
-{
+    style: { top: "25%", right: "10%", width: '25%' }
+  }, 
+    {
     src:'images/america/diner.jpg',
     alt:'wide shot of inside of american diner',
     caption:'1 hour before total eclipse. Welcome to Paradise',
     style:{top: "45%", right: "27%", width: '46%' }
   },
+  {
+    src:'images/america/cloudflag.jpg',
+    alt:'american flag in stormy weather',
+    caption:'Camping World Giant Flag',
+    style:{ top: "80%", left: "0%", width: '50%' }
+  },  
+  {
+    src:'images/america/animals.jpg',
+    alt:'Stuffed animals on rocks',
+    caption:'Hyper-reality',
+    style:{top: "70%", right: "0%", width: '30%' }
+  },
+  {
+    src:'images/america/powerstation.jpg',
+    alt:'people swimming on beach next to power station',
+    caption:'"Industrial Swimming," Dunkirk, NY.',
+    style:{top: "135%", left: "0%", width: '35%' }
+  },
+  {
+    src:'images/america/caution3.jpg',
+    alt:'caution tape on baby swing',
+    caption:'Kiddie Swing Police Line',
+    style:{top: "135%", right: "40%", width: '15%' }
+  },
+  {
+    src:'images/america/bowpeeps.jpg',
+    alt:'people on the bow of a boat',
+    caption:'Deck of the Badger Crossing Lake Michigan',
+    style:{top: "150%", right: "0%", width: '27%' }
+  },
+
 ]
 
         
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default () => {
     <div className ='photobox'>
         <Link href='/gallery5'><a><img style={{ top: "8%", right: "10%", width: '18%' }} src='images/DawnV_1.jpg' alt='woman jumping'></img></a></Link>
         <Link href='/gallery3'><a><img style={{ top: "25%", left: "52%", width: '18%' }} src='images/lotus.jpg' alt='lotus flower'></img></a></Link>
-        <Link href='/gallery8'><a><img style={{ top: "18%", left: "22%", width: '18%' }} src='images/Hail_Badger.jpg' alt='large ship'></img></a></Link>
+        <Link href='/gallery8'><a><img style={{ top: "18%", left: "22%", width: '18%' }} src='images/travels/plume.jpg' alt='large plume of steam'></img></a></Link>
         <Link href='/gallery1'><a><img style={{ top: "5%", left: "10%", width: '20%' }} src='images/MainStreet_flags_america.jpg' alt='pickup truck with many american flags'></img></a></Link>
         <Link href='/gallery6'><a><img style={{ top: "40%", left: "2%", width: '18%' }} src='images/shiny_bug_selfPortrait-6.jpg' alt='reflection in shiny beatle shell'></img></a></Link>
         <Link href='/gallery4'><a><img style={{ top: "65%", left: "16%", width: '17%' }} src='images/blackBird_contrail.jpg' alt='black bird sky and airplane contrail'></img></a></Link>
