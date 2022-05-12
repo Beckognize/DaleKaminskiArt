0496e9f899dc26f1cb324dc8df29e4dde9a5f885
Rebecca Lessem <beckognize@gmail.com>
2022-05-12 16:26:49 +0100

    added CV page and CV info in table

--- pages/contact.js
+++ pages/contact.js
@@ -1,24 +1,26 @@
-import Navbar from '../components/navbar'
-import Header from '../components/header'
-import Link from 'next/link'
+import Navbar from "../components/navbar";
+import Header from "../components/header";
+import Link from "next/link";
 export default () => {
-    return (
-        <>
-
-<Header/>
-<Navbar/>
-<body>
-  <div className = 'container-fluid'>
-    <div className = 'conbox'> 
-      <div className = 'wordbox'>
-       <div>
-          <h1>Contact me</h1>
-          <p className = 'a'>For inquiries, including the purchase of prints, please email <a href = "mailto:info@dalekimages.com">info@dalekimages.com</a></p>
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <body>
+        <div className="container-fluid">
+          <div className="conbox">
+            <div className="wordbox">
+              <div>
+                <h1>Contact me</h1>
+                <p className="a">
+                  For inquiries, including the purchase of prints, please email{" "}
+                  <a href="mailto:info@dalekimages.com">info@dalekimages.com</a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</body>
-</>
-);
-}
+      </body>
+    </>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,325 @@
-import Navbar from '../components/navbar'
-import Header from '../components/header'
-import Link from 'next/link'
+import Navbar from "../components/navbar";
+import Header from "../components/header";
+import Link from "next/link";
 export default () => {
-    return (
-        <>
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <body>
+        <div className="container-fluid">
+          <div className="frontbox">
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "5%", right: "0%", width: "100%" }}
+                  src="images/front/windmills.jpg"
+                  alt="windmills in distance"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "61%", left: "0%", width: "80%" }}
+                  src="images/front/panocabin.jpg"
+                  alt="panorama of cabin by lake"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "76%", left: "0%", width: "80%" }}
+                  src="images/front/panodeathvally.jpg"
+                  alt="panorama of cabin by lake"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery1">
+              <a>
+                <img
+                  style={{ top: "92%", left: "0%", width: "80%" }}
+                  src="images/front/flatroad.jpg"
+                  alt="panorama of flat road"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "55%", right: "0%", width: "15%" }}
+                  src="images/front/panovert.jpg"
+                  alt="vertical panorama of desert and sky"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "109%", left: "0%", width: "18%" }}
+                  src="images/front/Dawn1.jpg"
+                  alt="woman jumping"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "109%", left: "20%", width: "18%" }}
+                  src="images/front/Dawn3.jpg"
+                  alt="woman jumping"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "109%", left: "40%", width: "18%" }}
+                  src="images/front/Dawn2.jpg"
+                  alt="woman jumping"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "142%", left: "11%", width: "38%" }}
+                  src="images/front/3ladies.jpg"
+                  alt="three elderly chinese women"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "131%", right: "19%", width: "28%" }}
+                  src="images/front/studentshadow.jpg"
+                  alt="chinese students taking pictures of their shadows"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery5">
+              <a>
+                <img
+                  style={{ top: "172%", left: "5%", width: "21%" }}
+                  src="images/front/mask.jpg"
+                  alt="chinese students wearing mask and glasses"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery3">
+              <a>
+                <img
+                  style={{ top: "172%", right: "5%", width: "35%" }}
+                  src="images/front/lotus.jpg"
+                  alt="a lotus flower"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery3">
+              <a>
+                <img
+                  style={{ top: "189%", right: "0%", width: "32%" }}
+                  src="images/front/closeup.jpg"
+                  alt="close up of center of flower"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery3">
+              <a>
+                <img
+                  style={{ top: "183%", right: "35%", width: "35%" }}
+                  src="images/front/beeflower.jpg"
+                  alt="bee flying away from flower"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "207%", left: "2%", width: "50%" }}
+                  src="images/front/clouds.jpg"
+                  alt="large clouds over horizon"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "238%", left: "5%", width: "26%" }}
+                  src="images/front/farmwindmills.jpg"
+                  alt="road with farm and windmills in the distance"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery4">
+              <a>
+                <img
+                  style={{ top: "233%", right: "5%", width: "60%" }}
+                  src="images/front/pano19.jpg"
+                  alt="hill with hay bales "
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "267%", right: "25%", width: "19%" }}
+                  src="images/front/plume.jpg"
+                  alt="big plume of steam across snowy feild"
+                ></img>
+              </a>
+            </Link>
 
-<Header/>
-<Navbar/>
-<body>
-   <div className = 'container-fluid'>
-    <div className ='frontbox'>
-    <Link href='/gallery8'><a><img style={{ top: "5%", right: "0%", width: '100%' }} src='images/front/windmills.jpg' alt='windmills in distance'></img></a></Link>
-    <Link href='/gallery8'><a><img style={{ top: "61%", left: "0%", width: '80%' }} src='images/front/panocabin.jpg' alt='panorama of cabin by lake'></img></a></Link>
-    <Link href='/gallery8'><a><img style={{ top: "76%", left: "0%", width: '80%' }} src='images/front/panodeathvally.jpg' alt='panorama of cabin by lake'></img></a></Link>
-    <Link href='/gallery1'><a><img style={{ top: "92%", left: "0%", width: '80%' }} src='images/front/flatroad.jpg' alt='panorama of flat road'></img></a></Link>
-    <Link href='/gallery8'><a><img style={{ top: "55%", right: "0%", width: '15%' }} src='images/front/panovert.jpg' alt='vertical panorama of desert and sky'></img></a></Link>
-    <Link href='/gallery5'><a><img style={{ top: "109%", left: "0%", width: '18%' }} src='images/front/Dawn1.jpg' alt='woman jumping'></img></a></Link>
-    <Link href='/gallery5'><a><img style={{ top: "109%", left: "20%", width: '18%' }} src='images/front/Dawn3.jpg' alt='woman jumping'></img></a></Link>
-    <Link href='/gallery5'><a><img style={{ top: "109%", left: "40%", width: '18%' }} src='images/front/Dawn2.jpg' alt='woman jumping'></img></a></Link>
-    <Link href='/gallery5'><a><img style={{ top: "142%", left: "11%", width: '38%' }} src='images/front/3ladies.jpg' alt='three elderly chinese women'></img></a></Link>
-    <Link href='/gallery5'><a><img style={{ top: "131%", right: "19%", width: '28%' }} src='images/front/studentshadow.jpg' alt='chinese students taking pictures of their shadows'></img></a></Link>   
-    <Link href='/gallery5'><a><img style={{ top: "172%", left: "5%", width: '21%' }} src='images/front/mask.jpg' alt='chinese students wearing mask and glasses'></img></a></Link>   
-    <Link href='/gallery3'><a><img style={{ top: "172%", right: "5%", width: '35%' }} src='images/front/lotus.jpg' alt='a lotus flower'></img></a></Link>   
-    <Link href='/gallery3'><a><img style={{ top: "189%", right: "0%", width: '32%' }} src='images/front/closeup.jpg' alt='close up of center of flower'></img></a></Link>   
-    <Link href='/gallery3'><a><img style={{ top: "183%", right: "35%", width: '35%' }} src='images/front/beeflower.jpg' alt='bee flying away from flower'></img></a></Link>   
-    <Link href='/gallery8'><a><img style={{ top: "207%", left: "2%", width: '50%' }} src='images/front/clouds.jpg' alt='large clouds over horizon'></img></a></Link>   
-    <Link href='/gallery8'><a><img style={{ top: "238%", left: "5%", width: '26%' }} src='images/front/farmwindmills.jpg' alt='road with farm and windmills in the distance'></img></a></Link>   
-    <Link href='/gallery4'><a><img style={{ top: "233%", right: "5%", width: '60%' }} src='images/front/pano19.jpg' alt='hill with hay bales '></img></a></Link>   
-    <Link href='/gallery8'><a><img style={{ top: "267%", right: "25%", width: '19%' }} src='images/front/plume.jpg' alt='big plume of steam across snowy feild'></img></a></Link>   
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "254%", left: "28%", width: "33%" }}
+                  src="images/front/hudsonriver.jpg"
+                  alt="the hudson river valley"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery6">
+              <a>
+                <img
+                  style={{ top: "273%", left: "0%", width: "48%" }}
+                  src="images/front/walkingdead.jpg"
+                  alt="images of dale fishing and guy from walking dead"
+                ></img>
+              </a>
+            </Link>
 
-    <Link href='/gallery8'><a><img style={{ top: "254%", left: "28%", width: '33%' }} src='images/front/hudsonriver.jpg' alt='the hudson river valley'></img></a></Link>   
-    <Link href='/gallery6'><a><img style={{ top: "273%", left: "0%", width: '48%' }} src='images/front/walkingdead.jpg' alt='images of dale fishing and guy from walking dead'></img></a></Link>   
-
-    <Link href='/gallery6'><a><img style={{ top: "305%", left: "10%", width: '28%' }} src='images/front/treeshadow.jpg' alt='forrest with tree and dales shadow'></img></a></Link>   
-    <Link href='/gallery8'><a><img style={{ top: "261%", right: "5%", width: '16%' }} src='images/front/panovert2.jpg' alt='the hudson river valley'></img></a></Link>   
-    <Link href='/gallery1'><a><img style={{ top: "287%", right: "25%", width: '15%' }} src='images/front/fall.jpg' alt='man on ground woman on the phone '></img></a></Link>   
-    <Link href='/gallery8'><a><img style={{ top: "320%", right: "24%", width: '34%' }} src='images/front/jukejoint.jpg' alt='juke joint at 3 am'></img></a></Link>   
-    <Link href='/gallery1'><a><img style={{ top: "331%", right: "2%", width: '28%' }} src='images/front/flags.jpg' alt='pickup truck with american flags'></img></a></Link>   
-    <Link href='/gallery4'><a><img style={{ top: "345%", leftt: "5%", width: '28%' }} src='images/front/fireroad.jpg' alt='art picture made of sand and rocks'></img></a></Link>   
-    <Link href='/gallery4'><a><img style={{ top: "365%", left: "15%", width: '28%' }} src='images/front/sunset.jpg' alt='sunset over water reflects sideways view'></img></a></Link>   
-    <Link href='/gallery7'><a><img style={{ top: "358%", right: "24%", width: '28%' }} src='images/front/topocortez.jpg' alt='black and white images of landscape'></img></a></Link>   
-    <Link href='/gallery7'><a><img style={{ top: "350%", right: "0%", width: '20%' }} src='images/front/silosugar.jpg' alt='black and white picture of sugar silo'></img></a></Link>   
-    <Link href='/gallery7'><a><img style={{ top: "375%", right: "27%", width: '20%' }} src='images/front/coopsilo.jpg' alt='black and white image of silo with coop written on it'></img></a></Link>   
-    <Link href='/gallery2'><a><img style={{ top: "385%", left: "5%", width: '28%' }} src='images/front/sculpture.jpg' alt='sculpture and writing'></img></a></Link>   
-    <Link href='/gallery2'><a><img style={{ top: "427%", left: "5%", width: '28%' }} src='images/front/listen.jpg' alt='birdhouse and text'></img></a></Link>   
-    <Link href='/gallery2'><a><img style={{ top: "408%", right: "5%", width: '50%' }} src='images/front/plasticland.jpg' alt='band poster'></img></a></Link>   
-    <Link href='/gallery2'><a><img style={{ top: "475%", left: "0%", width: '100%' }} src='images/front/nytrain.jpg' alt='panorama of subway train'></img></a></Link>   
-
-    </div>
-    </div>
-</body>
-</>
-);
-}
+            <Link href="/gallery6">
+              <a>
+                <img
+                  style={{ top: "305%", left: "10%", width: "28%" }}
+                  src="images/front/treeshadow.jpg"
+                  alt="forrest with tree and dales shadow"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "261%", right: "5%", width: "16%" }}
+                  src="images/front/panovert2.jpg"
+                  alt="the hudson river valley"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery1">
+              <a>
+                <img
+                  style={{ top: "287%", right: "25%", width: "15%" }}
+                  src="images/front/fall.jpg"
+                  alt="man on ground woman on the phone "
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery8">
+              <a>
+                <img
+                  style={{ top: "320%", right: "24%", width: "34%" }}
+                  src="images/front/jukejoint.jpg"
+                  alt="juke joint at 3 am"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery1">
+              <a>
+                <img
+                  style={{ top: "331%", right: "2%", width: "28%" }}
+                  src="images/front/flags.jpg"
+                  alt="pickup truck with american flags"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery4">
+              <a>
+                <img
+                  style={{ top: "345%", leftt: "5%", width: "28%" }}
+                  src="images/front/fireroad.jpg"
+                  alt="art picture made of sand and rocks"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery4">
+              <a>
+                <img
+                  style={{ top: "365%", left: "15%", width: "28%" }}
+                  src="images/front/sunset.jpg"
+                  alt="sunset over water reflects sideways view"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery7">
+              <a>
+                <img
+                  style={{ top: "358%", right: "24%", width: "28%" }}
+                  src="images/front/topocortez.jpg"
+                  alt="black and white images of landscape"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery7">
+              <a>
+                <img
+                  style={{ top: "350%", right: "0%", width: "20%" }}
+                  src="images/front/silosugar.jpg"
+                  alt="black and white picture of sugar silo"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery7">
+              <a>
+                <img
+                  style={{ top: "375%", right: "27%", width: "20%" }}
+                  src="images/front/coopsilo.jpg"
+                  alt="black and white image of silo with coop written on it"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery2">
+              <a>
+                <img
+                  style={{ top: "385%", left: "5%", width: "28%" }}
+                  src="images/front/sculpture.jpg"
+                  alt="sculpture and writing"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery2">
+              <a>
+                <img
+                  style={{ top: "427%", left: "5%", width: "28%" }}
+                  src="images/front/listen.jpg"
+                  alt="birdhouse and text"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery2">
+              <a>
+                <img
+                  style={{ top: "408%", right: "5%", width: "50%" }}
+                  src="images/front/plasticland.jpg"
+                  alt="band poster"
+                ></img>
+              </a>
+            </Link>
+            <Link href="/gallery2">
+              <a>
+                <img
+                  style={{ top: "475%", left: "0%", width: "100%" }}
+                  src="images/front/nytrain.jpg"
+                  alt="panorama of subway train"
+                ></img>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+};
